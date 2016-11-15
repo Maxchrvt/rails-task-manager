498f27f16fbdb9c8aab4c331440c086aa05ca2db
Maxime Charvet <maximecharvet@hotmail.com>
2016-11-15 15:27:15 +0100

    ok

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  validates :name, presence: true
+  validates :done, inclusion: { in: [true, false] }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+wagon = Task.new(name: "lewagon", done: true, description: "aller au wagon")
+wagon.save!
